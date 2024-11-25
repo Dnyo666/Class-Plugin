1b93507e07599b965771f5fb7a5db1e334cd5598
Dnyo666 <dnyo666@foxmail.com>
2024-11-25 18:45:03 +0800

    哎哟，写不归一

--- components/Version.js
+++ components/Version.js
@@ -1,47 +1,27 @@
 import fs from 'node:fs'
 import { fileURLToPath } from 'url'
 import { join, dirname } from 'path'
-import plugin from '../../../lib/plugins/plugin.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-// 插件路径
 const pluginPath = join(__dirname, '..')
 const pluginName = 'class-plugin'
 
-let BotName = 'Unknown'
-let BotPath = ''
-let BotVersion = ''
-let pluginVersion = ''
+// 读取版本信息
+const packageJson = JSON.parse(fs.readFileSync(join(pluginPath, '../../package.json'), 'utf8'))
+const pluginPackageJson = JSON.parse(fs.readFileSync(join(pluginPath, 'package.json'), 'utf8'))
 
-try {
-  // 读取插件package.json
-  const pluginPackage = JSON.parse(fs.readFileSync(join(pluginPath, 'package.json'), 'utf8'))
-  pluginVersion = pluginPackage.version
-
-  // 读取Bot package.json
-  BotPath = join(pluginPath, '../..')
-  const BotPackage = JSON.parse(fs.readFileSync(join(BotPath, 'package.json'), 'utf8'))
-  BotVersion = BotPackage.version
-
-  // 判断Bot类型
-  if (BotPackage.name === 'miao-yunzai') {
-    BotName = 'Miao-Yunzai'
-  } else if (BotPackage.name === 'trss-yunzai') {
-    BotName = 'Trss-Yunzai'
-  } else if (BotPackage.name === 'yunzai-bot') {
-    BotName = 'Yunzai-Bot'
-  }
-} catch (err) {
-  console.error('[Class-Plugin] 读取package.json失败', err)
-}
+const BotVersion = packageJson.version
+const pluginVersion = pluginPackageJson.version
+const BotName = packageJson.name === 'miao-yunzai' ? 'Miao-Yunzai' : 
+               packageJson.name === 'trss-yunzai' ? 'Trss-Yunzai' : 
+               packageJson.name === 'yunzai-bot' ? 'Yunzai-Bot' : 'Unknown'
 
 export {
   pluginName,
   pluginPath,
   pluginVersion,
   BotName,
-  BotPath,
   BotVersion
 }
