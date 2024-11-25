db10c7be0d0c28092fa92fc6181b746cf82b8195
Dnyo666 <dnyo666@foxmail.com>
2024-11-25 22:32:55 +0800

    修复菜单-1

--- resources/help/config.js
+++ resources/help/config.js
@@ -1,6 +1,6 @@
 export const helpCfg = {
-  title: 'CLASS-PLUGIN 帮助',
-  subTitle: 'CLASS-PLUGIN HELP',
+  title: '课表插件帮助',
+  subTitle: 'Class-Plugin Help',
   colWidth: 265,
   colCount: 2,
   bgBlur: true
@@ -23,12 +23,12 @@ export const helpList = [
       {
         icon: 86,
         title: '#添加课程',
-        desc: '添加新课程'
+        desc: '格式：#添加课程 课程名 教师 教室 星期 节数 周数\n例：#添加课程 高数 张三 A101 周一 1-2 1-16周'
       },
       {
         icon: 14,
         title: '#删除课程',
-        desc: '删除指定课程'
+        desc: '格式：#删除课程 课程ID\n例：#删除课程 1'
       }
     ]
   },
@@ -37,23 +37,23 @@ export const helpList = [
     list: [
       {
         icon: 1,
-        title: '#调课',
-        desc: '临时调课'
+        title: '#修改课程',
+        desc: '格式：#修改课程 课程ID 属性=新值\n例：#修改课程 1 name=高等数学'
       },
       {
         icon: 5,
-        title: '#取消调课',
-        desc: '取消调课记录'
+        title: '#调课',
+        desc: '格式：#调课 课程ID 新节数\n例：#调课 1 3-4'
       },
       {
         icon: 7,
-        title: '#开启提醒',
-        desc: '开启上课提醒'
+        title: '#取消调课',
+        desc: '格式：#取消调课 课程ID\n例：#取消调课 1'
       },
       {
         icon: 11,
-        title: '#设置提醒时间',
-        desc: '设置提醒时间'
+        title: '#提醒设置',
+        desc: '#开启提醒\n#关闭提醒\n#设置提醒时间 分钟\n#切换提醒方式'
       }
     ]
   }
