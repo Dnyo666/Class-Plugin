c1fa77f43cc73c8ad4de321492ef0bf51a561f8b
Dnyo666 <dnyo666@foxmail.com>
2024-12-01 12:29:37 +0800

    fix：导入问题

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import fs from 'node:fs'
+import path from 'node:path'
 import { Config } from './model/config.js'
 
 // 确保必要的目录存在
 const _path = process.cwd()
-const pluginPath = _path + '/plugins/class-plugin'
+const pluginPath = path.join(_path, 'plugins/class-plugin')
 const tempPath = path.join(_path, 'temp')
 
+// 创建临时目录
 if (!fs.existsSync(tempPath)) {
   fs.mkdirSync(tempPath, { recursive: true })
 }
@@ -19,5 +21,11 @@ export * from './apps/Help.js'
 export * from './apps/Notify.js'
 
 // 检查更新
-let packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
-logger.info(`[Class-Plugin] 课表插件 v${packageJson.version} 初始化完成`)
+try {
+  const packagePath = path.join(pluginPath, 'package.json')
+  const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'))
+  logger.info(`[Class-Plugin] 课表插件 v${packageJson.version} 初始化完成`)
+} catch (err) {
+  logger.warn('[Class-Plugin] 无法读取版本信息')
+  logger.info('[Class-Plugin] 课表插件初始化完成')
+}
